517d3e12fa87b7c1a0457f5835c73c539a330c6c
Yaroslava Bykova <48364483+ibykova@users.noreply.github.com>
2021-02-05 15:54:24 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -11,3 +11,12 @@ function getAverageAge(list) {
   
   return Number(res.toFixed(0));
 }
+
+//Task 2 Decsending order (reversed number)
+//link: https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
+
+function descendingOrder(n){
+    return parseInt(n.toString().split('').sort().reverse().join(''), 10);
+  }
+console.log(descendingOrder(n));
+
